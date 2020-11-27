b345d5c840c2d0d4db36edbfb100a7c5958f0c44
Álvaro Rodrigues <69397692+alvarordgs@users.noreply.github.com>
2020-11-27 00:07:06 -0300

    Adição de arquivos por upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function limpar(){
+    document.getElementById('visor').value = ''
+}
+
+function preencher(valor){
+    document.getElementById('visor').value += valor
+}
+
+function calcular(){
+    let resultado = 0
+    resultado = document.getElementById('visor').value
+    document.getElementById('visor').value = ''
+    document.getElementById('visor').value = eval(resultado)
+}
